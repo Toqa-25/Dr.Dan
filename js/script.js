c087318bc4d4d79ecaa03a2cddef77e362c28795
Toqa-25 <toqa35abdelsalam757@gmail.com>
2022-08-12 03:10:30 +0200

    edit html & css file

--- js/script.js
+++ js/script.js
@@ -4,12 +4,10 @@ bar.onclick = function (){
     if (this.classList.contains("collapsed")){
         this.innerHTML = ``
         this.innerHTML = ` <i class=" fa-solid fa-bars bar-icon"></i>`
-        document.querySelector("hr").style.display = "block"
-
     }else{
         this.innerHTML = ``
-        this.innerHTML = `<i class="fa-solid fa-xmark bar-icon"></i>`
-        document.getElementsByTagName("hr").style.display = "none"
+        this.innerHTML = ` <i class="fas fa-times close-icon"></i>`
+        
     }
 }
 
@@ -19,15 +17,7 @@ let slider = document.querySelector(".slider"),
               "https://danneundorf.com/wp-content/uploads/2022/03/Booking-banner3.jpg" ,
               "https://danneundorf.com/wp-content/uploads/2022/03/imgl623568-2.jpg" ],
               index = 0;
-//   function random(){
-//     let randomNumber = Math.floor(Math.random() * imgsArray.length) ;
-//     // console.log(randomNumber)
-//     console.log(imgsArray[1].next())
-//      slider.style.backgroundImage=`url("${imgsArray[randomNumber]}")`;
-//       }
-    //   setInterval( random, 10000) 
-    //   random()
-// slider
+
 
 function dynamicSlider(){
     if (index >= 0 && index < imgsArray.length ){
